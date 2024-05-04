e8fa900ec78ac5900ddd48131e84227fededd97e
ebo_shit <isakovn2005@gmail.com>
2024-05-04 14:41:23 +0300

    server

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
+        filename="log.logging",
         format=u'%(filename)s:%(lineno)d #%(levelname)-3s [%(asctime)s] - %(message)s',
         filemode="w",
         encoding='utf-8')
