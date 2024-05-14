2436c106eae6d52917f0e92c2e52357d65785327
ebo_shit <isakovn2005@gmail.com>
2024-05-14 21:47:43 +0300

    add change balance and change rules

--- handlers/admins/balance.py
+++ handlers/admins/balance.py
@@ -22,25 +22,30 @@ async def admin(message: Message | CallbackQuery, state: FSMContext):
                                 text=get_mes("change_balance"),
                                 reply_markup=Keyboards.admin_back_menu_kb)
     await state.set_state(AdminStates.change_balance)
+    await state.update_data(message_id=message.message.message_id)
 
 
 @router.message(IsAdmin(), AdminStates.change_balance)
 async def change_balance(message: Message | CallbackQuery, state: FSMContext):
     id = message.from_user.id
+    data = await state.get_data()
+    message_id = data["message_id"]
+    await message.delete()
     if is_float(message.text):
         seller = await sellers.get()
         seller.balance -= float(message.text)
         await sellers.update(seller)
         await bot.edit_message_text(chat_id=id,
-                                    message_id=message.message.message_id,
+                                    message_id=message_id,
                                     text=get_mes("change_balance_complete"),
                                     reply_markup=Keyboards.admin_menu_kb)
         await state.clear()
     else:
-        await message.answer(
-            text=get_mes("err_cancel_err"),
-            reply_markup=Keyboards.admin_menu_kb
-        )
+        await bot.edit_message_text(chat_id=id,
+                                    message_id=message_id,
+                                    text=get_mes("err_cancel_err"),
+                                    reply_markup=Keyboards.admin_menu_kb
+                                    )
 
 
 balance_rt = router
