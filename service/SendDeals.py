1087ed9518405644f4fb9d87c554ee8bf53f6b71
ebo_shit <isakovn2005@gmail.com>
2024-08-02 01:43:00 +0300

    complete edits

--- service/SendDeals.py
+++ service/SendDeals.py
@@ -17,19 +17,13 @@ async def send(message: CallbackQuery, all_data_deals: list[DataDeals], keyboard
         await message.answer("У вас ещё нет совершенных сделок")
         return
     while ind < len(all_data_deals):
+
         data_deal = all_data_deals[ind]
+        ind += 1
         count_symbol += data_deal.len() + 130
         data_deal.price = rounding_numbers(str(data_deal.price))
-        ind += 1
         if count_symbol < 2000:
             new_data.append(data_deal)
-            # if ind == len(all_data_deals):
-            #     await bot.send_message(
-            #         chat_id=message.message.chat.id,
-            #         text=get_mes("history_buy", deals=new_data),
-            #         parse_mode=ParseMode.MARKDOWN_V2,
-            #         reply_markup=keyboard
-            #     )
         else:
             await bot.send_message(
                 chat_id=message.message.chat.id,
@@ -39,7 +33,6 @@ async def send(message: CallbackQuery, all_data_deals: list[DataDeals], keyboard
             count_symbol = 0
             new_data = list()
             new_data.append(data_deal)
-        # ind += 1
 
     if len(new_data) != 0:
         await bot.send_message(
@@ -48,8 +41,4 @@ async def send(message: CallbackQuery, all_data_deals: list[DataDeals], keyboard
             parse_mode=ParseMode.MARKDOWN_V2,
             reply_markup=keyboard
         )
-    # await bot.send_message(
-    #     chat_id=message.message.chat.id,
-    #     text=get_mes(fmes_text_path),
-    #     reply_markup=keyboard
-    # )
+
