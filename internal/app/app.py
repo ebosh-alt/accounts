7215f20d8022abf871e8de6bb24d80bc4804e290
ebo_shit <isakovn2005@gmail.com>
2025-02-19 02:02:48 +0300

    update ORM, schemas, models

--- internal/app/app.py
+++ internal/app/app.py
@@ -13,5 +13,4 @@ bot = Bot(config.telegram_bot.token)
 tg_acc = TG_Acc(session_name=config.telegram_client.session_path, api_id=config.telegram_client.api_id, api_hash=config.telegram_client.api_hash, phone_number=config.telegram_client.phone_number)
 client_s = TGClient_S(tg_acc)
 
-    
-
+# TODO: не работает подключение к базе данных(база данных существует)
