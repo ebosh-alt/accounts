42256e7e0a977c1c4fcff5548e7ee27f22ab277e
ebo_shit <isakovn2005@gmail.com>
2025-02-20 00:38:44 +0300

    update fast_api

--- config/config.py
+++ config/config.py
@@ -1,10 +1,7 @@
 import logging
-from config.settings.setting import Config
+from settings.setting import Config
 
 
 logger = logging.getLogger(__name__)
 
 config: Config = Config.load()
-
-logger.info(config.db)
-logger.info(config.admin)
